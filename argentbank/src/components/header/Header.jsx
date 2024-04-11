1d5496f122c27e3afac3bea06c7aa4529c432301
Lionel  Faure <faure_lionel@hotmail.fr>
2024-04-11 10:55:35 +0200

    Intégration du Header

--- argentbank/src/components/header/Header.jsx
+++ argentbank/src/components/header/Header.jsx
@@ -1,19 +1,23 @@
+import logo from '../../assets/img/argentBankLogo.png'
 import './header.scss'
 import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
-    <div className='main__nav'>
+    <div className='main-nav'>
       <a className='main-nav-logo' href='/'>
         <img
           className='main-nav-logo-image'
-          src='../../assets/img/argentBankLogo.png'
+          src={logo}
           alt='Argent Bank Logo'
         />
       </a>
-      <nav className=''>
-        <NavLink to='/' className='header__navlink'>
-          Accueil
+      <nav>
+        <NavLink to='/' className='main-nav-item'>
+          <FontAwesomeIcon icon={faCircleUser} />
+          Sign In
         </NavLink>
       </nav>
     </div>
